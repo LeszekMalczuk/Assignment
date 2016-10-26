de7667bab590c22ff34f5208e0fcf1322fd43dee
lehom <lehom@DESKTOP-P5U8BU7>
2016-10-26 11:08:23 +0100

    finish code of match and place in array

--- src/assignment1/BruteAutocomplete.java
+++ src/assignment1/BruteAutocomplete.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
 import org.junit.validator.PublicClassValidator;
 import org.omg.CORBA.PUBLIC_MEMBER;
+import org.w3c.dom.css.Counter;
 
 
 public class BruteAutocomplete implements AutoComplete 
 {
-	private Scanner input;
+
 	ArrayList<Term> listOfTerms = new ArrayList<>();
 	private double result;
 	private String result2;
@@ -43,7 +46,7 @@ public class BruteAutocomplete implements AutoComplete
 				if (s.termTerm.toLowerCase().equals(inputterm.toLowerCase()))
 				{
 					result =  s.weight;
-					System.out.println("wynik " +  s.weight +"\n");
+					//System.out.println("wynik " +  s.weight +"\n");
 
 				}
 
@@ -69,40 +72,38 @@ public class BruteAutocomplete implements AutoComplete
 		int count = 1;
 		for (Term s : listOfTerms) 
 		{
-
 			if (s.termTerm.toLowerCase().startsWith(prefix) && count ==1)
 			{
-				result2 =  s.termTerm;
-			//	System.out.println("wynik " +  result2 +"\n");
+				result2 =  s.termTerm;  //	System.out.println("wynik " +  result2 +"\n");
 				count--;
+			}
+		}
+		return result2;
+	}
 
+	@Override
+	public Iterable<String> matches(String prefix, int k) 
+	{
+		ArrayList<String> colectionK = new ArrayList<>();
+
+	int counter = 0;
+	
+		for (Term s : listOfTerms) 
+		{
+			if (s.termTerm.toLowerCase().startsWith(prefix) && counter++ < k)
+			{
+				colectionK.add(s.termTerm);
+				//colectionK.add(s.toString());
 			}
 
 		}
 
-	return result2;
-}
+		return colectionK;
 
-
-
-@Override
-public Iterable<String> matches(String prefix, int k) 
-{
-	// TODO Auto-generated method stub
-	return null;
-	// napisanie kodu do porównywania  string - int 
-}
-
-
-
-
-
-
-
-
-
-
-
+		// TODO Auto-generated method stub
+		//return null;
+		// napisanie kodu do porównywania  string - int 
+	}
 
 
 }
--- src/assignment1/Cilent.java
+++ src/assignment1/Cilent.java
@@ -1,13 +1,34 @@
 package assignment1;
 
+import java.util.Scanner;
+
 public class Cilent
 {
 
 	public static void main (String[] args)
 	{
 		BruteAutocomplete bruteAutocomplete = new BruteAutocomplete();
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println(bruteAutocomplete.bestMatch("the"));
+		System.out.println("Enter prefix to find the weight of the word in the array: ");
+		String input = scanner.nextLine();
+		System.out.println("The weight of " + input +" is " + bruteAutocomplete.weightOf(input)+ "\n");
+		
+		System.out.println("Enter prefix or leter to best match to words in array: ");
+		String input1 = scanner.nextLine();
+		System.out.println(bruteAutocomplete.bestMatch(input1));
+		System.out.println("The best match of " + input1 +" is " + bruteAutocomplete.bestMatch(input1)+ "\n");
+		
+		
+		System.out.println("Enter prefix or leter to search in the list: ");
+		String input3 = scanner.nextLine();
+		System.out.println("Enter a number how many words placed in array: ");
+		int input4 = scanner.nextInt();
+		//System.out.println(bruteAutocomplete.matches(input3, input4));
+		System.out.println("The best match of " + " in the array is " + bruteAutocomplete.matches(input3,input4)+ "\n");
+		
+		
+		
 	}
 }
-// i need a scanner for input data
+
